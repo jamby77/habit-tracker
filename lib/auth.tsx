e0b37ae2f27025023f5779743c28d4f120009fca
Petar Dzhambazov <jamby77@gmail.com>
2021-01-05 16:58:47 +0200

    add base firebase setup

---
+++ lib/auth.tsx
@@ -0,0 +1,92 @@
+import React, { useState, useEffect, useContext, createContext } from "react";
+import queryString from "query-string";
+import firebase from "./firebase";
+
+const authContext = createContext(null);
+
+export const ProvideAuth: React.FC = ({ children }) => {
+  const auth = useProvideAuth();
+  return <authContext.Provider value={auth}>{children}</authContext.Provider>;
+};
+
+export const useAuth = () => {
+  return useContext(authContext);
+};
+
+function useProvideAuth() {
+  const [user, setUser] = useState(null);
+
+  const signin = (email, password) => {
+    return firebase
+      .auth()
+      .signInWithEmailAndPassword(email, password)
+      .then((response) => {
+        setUser(response.user);
+        return response.user;
+      });
+  };
+
+  const signup = (email, password) => {
+    return firebase
+      .auth()
+      .createUserWithEmailAndPassword(email, password)
+      .then((response) => {
+        setUser(response.user);
+        return response.user;
+      });
+  };
+
+  const signout = () => {
+    return firebase
+      .auth()
+      .signOut()
+      .then(() => {
+        setUser(false);
+      });
+  };
+
+  const sendPasswordResetEmail = (email) => {
+    return firebase
+      .auth()
+      .sendPasswordResetEmail(email)
+      .then(() => {
+        return true;
+      });
+  };
+
+  const confirmPasswordReset = (password, code) => {
+    const resetCode = code || getFromQueryString("oobCode");
+
+    return firebase
+      .auth()
+      .confirmPasswordReset(resetCode, password)
+      .then(() => {
+        return true;
+      });
+  };
+
+  useEffect(() => {
+    const unsubscribe = firebase.auth().onAuthStateChanged((user) => {
+      if (user) {
+        setUser(user);
+      } else {
+        setUser(false);
+      }
+    });
+
+    return () => unsubscribe();
+  }, []);
+
+  return {
+    user,
+    signin,
+    signup,
+    signout,
+    sendPasswordResetEmail,
+    confirmPasswordReset,
+  };
+}
+
+const getFromQueryString = (key) => {
+  return queryString.parse(window.location.search)[key];
+};
