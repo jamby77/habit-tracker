f4ac73a7ca18970049c1450348d2827b05b9514e
Petar Dzhambazov <petar.dzhambazov@ezbob.com>
2021-01-11 11:28:43 +0200

    update favicons for many platforms

--- components/Layout.tsx
+++ components/Layout.tsx
@@ -1,5 +1,5 @@
-import React from "react";
 import Head from "next/head";
+import React from "react";
 import { useLayout } from "../lib/layout";
 import HeadingMobile from "./HeadingMobile";
 import Messages from "./Messages";
@@ -10,7 +10,27 @@ const Layout: React.FC = ({ children }) => {
     <>
       <Head>
         <title>{title}</title>
-        <link rel="icon" href="/favicon.ico" />
+        <link
+          rel="apple-touch-icon"
+          sizes="180x180"
+          href="/apple-touch-icon.png"
+        />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="32x32"
+          href="/favicon-32x32.png"
+        />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="16x16"
+          href="/favicon-16x16.png"
+        />
+        <link rel="manifest" href="/site.webmanifest" />
+        <link rel="mask-icon" href="/safari-pinned-tab.svg" color="#5bbad5" />
+        <meta name="msapplication-TileColor" content="#da532c" />
+        <meta name="theme-color" content="#ffffff" />
       </Head>
       <HeadingMobile />
       <Messages />
