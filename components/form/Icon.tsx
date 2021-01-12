48054ace20c2771335c4322ee1b5067e9e4ad312
Petar Dzhambazov <petar.dzhambazov@ezbob.com>
2021-01-12 14:32:35 +0200

    changed cells design

--- components/form/Icon.tsx
+++ components/form/Icon.tsx
@@ -1,9 +1,15 @@
+import cn from "clsx";
 import React from "react";
 
-const Icon = ({ icon }) => {
+const Icon = ({ icon, className = "", color = "" }) => {
+  const iconClass = cn(
+    "pointer-events-none flex items-center justify-center",
+    className
+  );
+  console.log(className, iconClass);
   return (
-    <div className="w-10 z-10 pl-1 text-center pointer-events-none flex items-center justify-center">
-      <i className={`mdi ${icon} text-gray-400 text-lg`} />
+    <div className={iconClass}>
+      <i className={`mdi ${icon} ${color}`} />
     </div>
   );
 };
