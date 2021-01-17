76de7bf1d4548b907f99579cd5e9dac1c4a8b6f5
Petar Dzhambazov <jamby77@gmail.com>
2021-01-17 17:38:23 +0200

    setup basic testing

--- __tests__/HomeTest.js
+++ __tests__/HomeTest.js
@@ -2,7 +2,14 @@ import Home from "../pages/index";
 import { render, screen } from "@testing-library/react";
 import "@testing-library/jest-dom/extend-expect";
 
-test("Render home page", () => {
-  const { getByText } = render(<Home />);
-  expect(getByText("Habit tracker")).toBeInTheDocument();
+describe("Home page", () => {
+  it("Renders home page", () => {
+    const { getByText } = render(<Home />);
+    expect(getByText("Habit tracker")).toBeInTheDocument();
+  });
+
+  it("should render todo", function () {
+    render(<Home />);
+    expect(screen.getByText("TODO: home page here")).toBeInTheDocument();
+  });
 });
